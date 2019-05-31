0afcde2a5cd97c6ce2f5cdabfb396561660f5dd1
caiojvg <caiojvg@gmail.com>
2019-05-31 20:43:55 -0300

    Salva dados em Shared Preferences

--- app/src/main/java/com/example/homecoming/RegisterActivity.kt
+++ app/src/main/java/com/example/homecoming/RegisterActivity.kt
@@ -1,11 +1,13 @@
 package com.example.homecoming
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.location.Geocoder
 import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -18,14 +20,23 @@ import com.google.android.gms.maps.model.MarkerOptions
 class RegisterActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var addressLatLng: LatLng
+    private var preferenceKey = 0
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        var address = findViewById<EditText>(R.id.search_address)
-        address.setOnFocusChangeListener { _, hasFocus -> searchAddress(address, hasFocus) }
+        var addressInput = findViewById<EditText>(R.id.search_address)
+        addressInput.setOnFocusChangeListener { _, hasFocus -> searchAddress(addressInput, hasFocus) }
+
+        sharedPref = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+
+        for (entry in sharedPref.all.entries) {
+            preferenceKey++
+        }
 
     }
 
@@ -33,26 +44,27 @@ class RegisterActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
     }
 
-    private fun searchAddress(address: EditText, hasFocus: Boolean){
+    private fun searchAddress(addressText: EditText, hasFocus: Boolean){
         if (!hasFocus){
             mMap.clear()
 
-            if (isConnectedtoInternet(this)){
-                var addressString = address.text.toString()
-                if(!addressString.isEmpty()){
-                    Log.i("Teste", "oi$addressString 1")
+            if (isConnectedToInternet(this)){
+                var addressString = addressText.text.toString()
+
+                if(addressString.isNotEmpty()){
                     var coder = Geocoder(this)
-                    Log.i("Teste", "olá")
                     var locationList = coder.getFromLocationName(addressString, 1)
-                    Log.i("Teste", locationList.toString())
+
                     if(locationList.isEmpty()){
                         Toast.makeText(this, "Endereço não encontrado, por favor tente novamente", Toast.LENGTH_LONG).show()
+
                     } else {
                         var location = locationList.first()
 
-                        var point = LatLng(location.latitude, location.longitude)
-                        mMap.addMarker(MarkerOptions().position(point).title(location.getAddressLine(0)))
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 10.0f))
+                        addressLatLng = LatLng(location.latitude, location.longitude)
+                        mMap.addMarker(MarkerOptions().position(addressLatLng).title(location.getAddressLine(0)))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(addressLatLng, 10.0f))
+
                     }
                 } else{
                     Toast.makeText(this, "Por favor, digite um endereço", Toast.LENGTH_LONG).show()
@@ -63,10 +75,22 @@ class RegisterActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun isConnectedtoInternet(context: Context): Boolean {
+    private fun isConnectedToInternet(context: Context): Boolean {
 
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         return cm.activeNetworkInfo != null
+    }
+
+    fun registerData(v: View){
+        var address = addressLatLng.toString()
+        var tag = findViewById<EditText>(R.id.tag).text.toString()
+        var phone = findViewById<EditText>(R.id.phone).text.toString()
+        var distance = findViewById<EditText>(R.id.distance).text.toString()
+        var list = setOf(address, tag, phone, distance)
+
+        val editor = sharedPref.edit()
+        editor.putStringSet(preferenceKey.toString(), list)
+        editor.commit()
     }
 
 }
